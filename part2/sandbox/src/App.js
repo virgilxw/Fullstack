71f80740224a98933e27616baabcb8730c65cf52
virgilxw <reubenwxw@gmail.com>
2022-02-14 17:42:13 +0000

    2.1-2.5 done

---
+++ part2/sandbox/src/App.js
@@ -0,0 +1,19 @@
+import { getNodeText } from "@testing-library/react"
+
+import Note from './components/Note.js'
+
+const App = ({notes}) => {
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+      {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
